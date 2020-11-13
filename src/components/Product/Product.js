9a6aab5d75e1abce72e3e5bf7b6ad8488085c744
unknown <punithmatli@gmail.com>
2020-11-13 07:41:19 +0530

    Added bootstrap button in Product component to add products to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,7 +7,6 @@ import './Product.css'
 const Product = () => {
     return (
         <div className="product">
-            <Col xs={1} md={1}>
             <div className="product__info">
                 <div>The Lean Startup</div>
                 <div className="product__price">
@@ -19,8 +18,7 @@ const Product = () => {
                 </div>
             </div>
             <img className="product__img" src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" alt="" /> <br />
-            <Button variant="warning" size="sm">Add to cart</Button>
-            </Col>
+            <Button className="product__button" variant="warning" size="sm">Add to cart</Button>
         </div>
     )
 }
