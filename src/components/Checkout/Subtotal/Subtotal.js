223a65a573eeedb65d24ecf5f4c0331c9e88df3f
unknown <punithmatli@gmail.com>
2020-11-14 18:13:06 +0530

    Used Currency Format from 'react-currency-format' package to display the amount in Subtotal component

--- src/components/Checkout/Subtotal/Subtotal.js
+++ src/components/Checkout/Subtotal/Subtotal.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import CurrencyFormat from 'react-currency-format';
 
 import './Subtotal.css'
 
 const Subtotal = () => {
     return <div className="subtotal">
-            <big>Subtotal ({3} items): <strong>Rs.{255}</strong></big>
+            <CurrencyFormat
+            renderText={(value) => (
+                <>
+                    <div>
+                        Subtotal (0 items): <strong>0</strong>
+                    </div>
+                    <small className="subtotal__gift">
+                        <input type="checkbox" />
+                          &nbsp;This order contains a gift
+                    </small>
+                </>
+            )}
+                decimalScale={2}
+                value={0}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"Rs."}
+            />
+<button>Proceed to Checkout</button>
+
+
+            {/* <big>Subtotal ({3} items): <strong>Rs.{255}</strong></big>
             <br /><br />
-            <p><input type="checkbox" /> This order contains a gift</p>
-            <Button variant="warning" block className="checkout__button">Proceed to Checkout</Button>
+            <p><input type="checkbox" /> This order contains a gift</p> */}
         </div>
     
 }
